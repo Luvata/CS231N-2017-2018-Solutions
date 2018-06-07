597aeed87f7f1f5115573969f43af84a83172063
Luvata <thanhlvse04819@gmail.com>
2018-06-08 00:40:04 +0700

    solved SoftMax and add additional explain

--- 2017/solutions/assignment1/cs231n/classifiers/softmax.py
+++ 2017/solutions/assignment1/cs231n/classifiers/softmax.py
@@ -32,15 +32,18 @@ def softmax_loss_naive(W, X, y, reg):
   #############################################################################
   num_trains = X.shape[0]
   num_classes = W.shape[1]
+  dA = np.zeros((num_trains,num_classes))
   for i in xrange(num_trains):
     dot = np.dot(X[i],W)
     dot -= np.max(dot)
     exp = np.exp(dot)
     norm = exp/ np.sum(exp)
     loss -= np.log(norm[y[i]])
-    
-    
-
+    norm[y[i]] -= 1
+    dA[i] = norm
+  dW = X.T.dot(dA)
+  dW /= num_trains
+  dW += 2 * reg * W
   ################## MUST NOT FORGET
   loss/=num_trains #
   ##################
@@ -69,10 +72,26 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
+  num_trains = X.shape[0]
+  num_classes = W.shape[1]
+  activation_all = np.dot(X,W)
+  max_row = np.max(activation_all,axis=1)
+  activation_all = (activation_all.T - max_row).T
+  exp = np.exp(activation_all)
+  norm = (exp.T / np.sum(exp,axis=1)).T # NxC
+  loss -= np.sum(np.log(norm[np.arange(num_trains),y]))
+  norm[range(num_trains),y] -= 1
+  dW = X.T.dot(norm)
+
+
+
   pass
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
-
+  dW /= num_trains
+  dW += 2 * reg * W
+  loss /= num_trains
+  loss += reg * np.sum(W*W)
   return loss, dW
 
